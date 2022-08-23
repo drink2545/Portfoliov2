fedaf9b859c51038e53e56550810dfc0e989e00c
Drink <drink2545@gmail.com>
2022-08-23 18:48:22 +0700

    beta1

--- JS/main_script.js
+++
@@ -1,8 +0,0 @@
-function navbar() {
-    var x = document.getElementById("navbarcontent");
-    if (x.className === "flex_container navbarcontent") {
-        x.classList.add("responsive")
-    } else {
-        x.classList.remove("responsive")
-    }
-}
